68ef1781ed55f2a18d5ff3465c83daed6ddaea83
Marcus Boay <marcusboay@hotmail.com>
2018-11-17 13:33:32 -0700

    make temp 2d array maze

--- template.py
+++ template.py
@@ -1,21 +1,33 @@
 import pygame
 from pygame.locals import *
 import time
+from random import randint
 
 
 def main():
+    # test data structure for maze
+    maze = [["" for i in range(10)] for j in range(10)]
+    startPoint = (randint(0, 9), randint(0, 9))
+    endPoint = (randint(0, 9), randint(0, 9))
+    maze[startPoint[0]][startPoint[1]] = "P"
+    maze[endPoint[0]][endPoint[1]] = "E"
+
     # Initialise screen
     pygame.init()
     screen = pygame.display.set_mode((1000, 500))
     pygame.display.set_caption('Basic Pygame program')
+    clock = pygame.time.Clock()
 
     # Fill background
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((250, 250, 250))
 
-    # wall
+    # Wall
     wall = pygame.Rect(10, 10, 10, 10)
+
+    # Grid lines
+    
 
     # Display some text
     font = pygame.font.Font(None, 36)
@@ -36,9 +48,10 @@ def main():
                 return
 
         screen.blit(background, (0, 0))
-		
+        pygame.draw.rect(screen, (0, 0, 0), wall)
         pygame.display.flip()
-        time.sleep(1)
+        clock.tick(60)
+        wall.move_ip(1, 1)
 
 
 if __name__ == '__main__':
